a2d62d656083ab86b89c39e2152997dbe609d44e
David Schmitz <david.schmitz.privat@gmail.com>
2015-10-13 13:00:15 +0200

    import

---
+++ src/main/java/org/koenighotze/pdftool/PdfTool.java
@@ -0,0 +1,104 @@
+package org.koenighotze.pdftool;
+
+import static java.lang.String.format;
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.newInputStream;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.AcroFields;
+import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfStamper;
+
+/**
+ * Simple tool for pre-stamping a PDF form with the keys of the fields as their
+ * value.
+ *
+ * @author dschmitz
+ */
+public class PdfTool {
+    private final Path document;
+
+    public PdfTool(Path path) {
+        this.document = path;
+    }
+
+    public static void main(String[] args) throws IOException, DocumentException {
+        if (args.length != 1) {
+            printUsage();
+
+            return;
+        }
+
+        Path path = Paths.get(args[0]);
+        if (!exists(path)) {
+            System.err.println(format("File %s does not exist!", path.getFileName()));
+
+            return;
+        }
+
+        new PdfTool(path).printPreFilledPdf();
+    }
+
+    private byte[] prefill() throws IOException, DocumentException {
+        PdfReader pdfReader = null;
+        PdfStamper stamper = null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            pdfReader = new PdfReader(newInputStream(document));
+            stamper = new PdfStamper(pdfReader, baos);
+            stamper.setFormFlattening(true);
+
+            AcroFields fields = stamper.getAcroFields();
+            @SuppressWarnings("unchecked") Set<String> set = fields.getFields().keySet();
+            set.stream().forEach(key -> {
+                try {
+                    fields.setField(key, key);
+                } catch (IOException | DocumentException e) {
+                    System.err.println(format("Cannot stamp field %s", key));
+                }
+            });
+            baos.flush();
+        } finally {
+            closeStamper(stamper);
+            closeReader(pdfReader);
+        }
+
+        return baos.toByteArray();
+    }
+
+    private void closeReader(PdfReader pdfReader) {
+        if (pdfReader != null) {
+            pdfReader.close();
+        }
+    }
+
+    private void closeStamper(PdfStamper stamper) {
+        if (stamper != null) {
+            try {
+                stamper.close();
+            } catch (DocumentException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void printPreFilledPdf() throws IOException, DocumentException {
+        byte[] doc = prefill();
+
+        Path out = Files.createTempFile("stamped", ".pdf");
+        Files.write(out, doc, WRITE);
+        System.out.println(format("Result is here: %s", out.toAbsolutePath()));
+    }
+
+    private static void printUsage() {
+        System.out.printf("Usage: PdfTool <file.pdf>");
+    }
+}
