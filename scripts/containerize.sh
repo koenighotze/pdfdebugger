620cd666cc0fa4616c5566c6f85c795bd0ff1ae9
David Schmitz <david.schmitz.privat@gmail.com>
2023-11-01 10:14:26 +0100

    Build image

--- scripts/containerize.sh
+++ scripts/containerize.sh
@@ -13,6 +13,8 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 : "${GITHUB_SERVER_URL?'Expected env var GITHUB_SERVER_URL not set'}"
 : "${GITHUB_REPOSITORY?'Expected env var GITHUB_REPOSITORY not set'}"
 : "${CONTAINER_REGISTRY?'Expected env var CONTAINER_REGISTRY not set'}"
+: "${DOCKER_USERNAME?'Expected env var DOCKER_USERNAME not set'}"
+: "${DOCKER_PASSWORD?'Expected env var DOCKER_PASSWORD not set'}"
 : "${CONTAINER_PORTS:=8080}"
 : "${OUTPUT_MODE:=}"
 
@@ -42,9 +44,9 @@ echo "$DOCKER_BUILD_OPTIONS"
   package \
   "jib:build${OUTPUT_MODE}" \
   -Dorg.opencontainers.image.revision="${GITHUB_SHA}" \
-  -Ddocker.tag=${GITHUB_REF#refs/tags/} \
-  -Djib.to.auth.username=${{ secrets.DOCKER_USERNAME }} \
-  -Djib.to.auth.password=${{ secrets.DOCKER_PASSWORD }}
+  -Ddocker.tag="${GITHUB_REF#refs/tags/}" \
+  -Djib.to.auth.username="${DOCKER_USERNAME}" \
+  -Djib.to.auth.password="${DOCKER_PASSWORD}"
 
 if [[ "$GITHUB_REF" = refs/tags/* ]]; then
     # shellcheck disable=SC2086
