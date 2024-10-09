e893c9952f7b6c4d6fd4842e527078706b8224db
David Schmitz <david.schmitz.privat@gmail.com>
2024-10-09 18:09:50 +0200

    Login to docker

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -13,7 +13,9 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 : "${GITHUB_REF?'Expected env var GITHUB_REF not set'}"
 : "${IMAGE_NAME?'Expected env var IMAGE_NAME not set'}"
 : "${IMAGE_TAR?'Expected env var IMAGE_TAR not set'}"
-: "${GITHUB_SHA?'Expected env var GITHUB_SHA not set'}"
+: "${GITHUB_SHA?'Expected env var GITHUB_SHA not set'
+: "${DOCKER_USERNAME?'Expected env var DOCKER_USERNAME not set'
+: "${DOCKER_PASSWORD?'Expected env var DOCKER_PASSWORD not set'}"
 
 if [[ "$GITHUB_REF" = refs/tags/* ]]; then
     GIT_TAG=${GITHUB_REF/refs\/tags\/}
@@ -23,6 +25,8 @@ else
 fi
 
 echo "Pushing image ${IMAGE_NAME} contained in ${IMAGE_TAR} to ${CONTAINER_REGISTRY}/${GITHUB_REPOSITORY}:${GIT_TAG}"
+
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 docker load --input "$IMAGE_TAR"
 docker tag "$IMAGE_NAME" "$CONTAINER_REGISTRY/$GITHUB_REPOSITORY:$GIT_TAG"
